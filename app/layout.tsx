edc4f3dfb4fe42a23705fe498326fc10e6e55c23
Keiana Hanson <kikihanson94@gmail.com>
2024-12-20 19:44:55 -0500

    made changes to resolve hydration issues from support inquiry

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,29 +1,30 @@
 import {
-    ClerkProvider,
-    SignInButton,
-    SignedIn,
-    SignedOut,
-    UserButton
-  } from '@clerk/nextjs'
-  import './globals.css'
-  export default function RootLayout({
-    children,
-  }: {
-    children: React.ReactNode
-  }) {
-    return (
-      <ClerkProvider>
-        <html lang="en">
-          <body>
-            <SignedOut>
-              <SignInButton />
-            </SignedOut>
-            <SignedIn>
-              <UserButton />
-            </SignedIn>
-            {children}
-          </body>
-        </html>
-      </ClerkProvider>
-    )
-  }
+  ClerkProvider,
+  SignInButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from "@clerk/nextjs";
+import "./globals.css";
+import HomePage from "./page";
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <ClerkProvider>
+      <html lang="en">
+        <body>
+          <SignedOut>
+            <p>Sign In Below</p>
+          </SignedOut>
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+          <main>{children}</main>
+        </body>
+      </html>
+    </ClerkProvider>
+  );
+}
